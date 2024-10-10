7c68feb66a9ae9061544e9c4e712158885af3f0f
navid bahrami <navidbahrami1996@gmail.com>
2024-10-10 14:05:50 +0330

    first commit

---
+++ app/server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const {default : mongoose} = require('mongoose')
+const path = require('path')
+const { Allroutes } = require('./router/router')
+
+module.exports = class application {
+    #app = express()
+    #DB_URI
+    #PORT
+    constructor(PORT , DB_URI){
+        this.#PORT = PORT
+        this.#DB_URI =DB_URI
+        this.configApplication();
+        this.connectToMongoDB();
+        this.createServer();
+        this.createRoutes();
+        this.errorHandling();
+    }
+    configApplication(){
+        this.#app.use(express.json())
+        this.#app.use(express.urlencoded({extends : true}))
+        this.#app.use(express.static(path.join(__dirname , '..' , 'public')))
+
+    }
+    createServer(){
+        const http = require('http')
+        http.createServer(this.#app).listen(this.#PORT, () => {
+            console.log('run > http://localhost:' + this.#PORT);
+        })
+    }
+    connectToMongoDB(){
+        mongoose.connect(this.#DB_URI , (error) => {
+            if(!error) return console.log('connect to mongodb')
+            return console.log('failed to connection');
+        })
+    }
+    createRoutes(){
+        this.#app.use(Allroutes)
+
+    }
+    errorHandling(){
+        this.#app.use((req ,res , next) => {
+            return res.status(404).json({
+                statusCode : 404 ,
+                message : 'page not found'
+            })
+        })
+        this.#app.use((error , req , res , next) => {
+            const statusCode = error.status || 500;
+            const message = error.message || 'internalServerError';
+            return res.status(statusCode).json({
+                statusCode ,
+                message
+            })
+        })
+    }
+}

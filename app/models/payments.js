28bf9d919af9c1f630d6e13122bfbd6d4c7b0566
navid bahrami <navidbahrami1996@gmail.com>
2024-10-14 12:17:43 +0330

    first commit

--- app/models/payments.js
+++ app/models/payments.js
@@ -1,9 +1,9 @@
 const { default: mongoose } = require("mongoose");
 
 const Schema = new mongoose.Schema({
-    title : {type : String , required : true}
+
 })
 
 module.exports = {
-    CategoryModel : mongoose.model('category' , Schema)
+    PaymentModel : mongoose.model('payment' , Schema)
 }

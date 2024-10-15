194fb4e360e5d90307732a943fa15dd877295f63
navid bahrami <navidbahrami1996@gmail.com>
2024-10-15 17:09:46 +0330

    first commit

---
+++ app/models/users.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+const { default: mongoose } = require("mongoose");
+
+const Schema = new mongoose.Schema({
+    first_name : {type : String} ,
+    last_name : {type : String} ,
+    username : {type : String , lowercase: true} ,
+    mobile : {type : String , required : true } ,
+    email : {type : String , lowercase : true} ,
+    password : {type : String} ,
+    otp : {type : Object , default : {
+        code : 0 ,
+        expiresIn : 0
+    }} ,
+    bills : {type : [] , default : []} ,
+    discount : {type : Number , default : 0} ,
+    birthday : {type : String} ,
+    roles  : {type : [String] , default : ['USER']}
+
+})
+
+module.exports = {
+    UserModel : mongoose.model('user' , Schema)
+}

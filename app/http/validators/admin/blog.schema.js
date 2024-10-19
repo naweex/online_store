edfd5d22b8da97118f89524ffcbccd28ba8d9a0c
navid bahrami <navidbahrami1996@gmail.com>
2024-10-19 12:22:28 +0330

    blog Schema

--- app/http/validators/admin/blog.schema.js
+++ app/http/validators/admin/blog.schema.js
@@ -1,5 +1,8 @@
 const Joi = require('joi')
 
 const createBlogSchema = Joi.object({
-    title : Joi.string().min(3).max(30).error(new Error('the title of category not accepted'))
+    title : Joi.string().min(3).max(30).error(new Error('the title of category not accepted')) ,
+    text : Joi.string().error(new Error('text you send is not accepted')) ,
+    short_text : Joi.string().error(new Error('text you send is not accepted')) ,
+    image : Joi.string().error(new Error('image you send is not accepted')) ,
 })

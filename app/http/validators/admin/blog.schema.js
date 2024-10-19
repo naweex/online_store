e0c3066883351e03fd1428d1d12eab16e622103c
navid bahrami <navidbahrami1996@gmail.com>
2024-10-19 12:26:57 +0330

    blog Schema

--- app/http/validators/admin/blog.schema.js
+++ app/http/validators/admin/blog.schema.js
@@ -1,9 +1,10 @@
 const Joi = require('joi')
-
+const { MongoIDPattern } = require('../../../utils/constance')
 const createBlogSchema = Joi.object({
     title : Joi.string().min(3).max(30).error(new Error('the title of category not accepted')) ,
     text : Joi.string().error(new Error('text you send is not accepted')) ,
     short_text : Joi.string().error(new Error('text you send is not accepted')) ,
     image : Joi.string().error(new Error('image you send is not accepted')) ,
     tags : Joi.array().min(0).max(20).error(new Error('tags should not over 20 character')) ,
-})
+    category : Joi.string().pattern(MongoIDPattern).error(new Error('category not found'))
+});

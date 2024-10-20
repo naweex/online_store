b857e1fb919a04fd48a9f5d9d904f7d2e15dfa57
navid bahrami <navidbahrami1996@gmail.com>
2024-10-20 12:14:18 +0330

    blog controller

--- app/http/controllers/admin/blog.controllers.js
+++ app/http/controllers/admin/blog.controllers.js
@@ -1,10 +1,20 @@
-const blogs = require('../../../models/blogs')
+const { BlogModel } = require('../../../models/blogs')
+const { createBlogSchema } = require('../../validators/admin/blog.schema')
 const Controller = require('./../controller')
+const path = require('path')
+const { deleteFileInPublic } = require('../../../utils/functions')
 class BlogController extends Controller {
     async createBlog(req , res , next){
         try {
-            
+            const blogDataBody = await createBlogSchema.validateAsync(req.body);
+            req.body.image = path.join(blogDataBody.fileUploadPath , blogDataBody.filename)
+            req.body.image = req.body.image.replace(/\\/g , '/')
+            const {title , text , short_text , category , tags} = blogDataBody;
+            const image  = req.body.image
+            const blog = await BlogModel.create({title , image , text , short_text , category , tags})
+            return res.json({blog})
         } catch (error) {
+            deleteFileInPublic(req.body.image)
             next(error)
         }
     }

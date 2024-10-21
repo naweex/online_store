1eae1e45f8ccecb6652ca4680a7873b020b1156c
navid bahrami <navidbahrami1996@gmail.com>
2024-10-21 11:34:11 +0330

    blog schema

--- app/http/validators/admin/blog.schema.js
+++ app/http/validators/admin/blog.schema.js
@@ -1,17 +1,16 @@
 const Joi = require('joi')
-const { MongoIDPattern } = require('../../../utils/constance')
+const { MongoIDPattern } = require('../../../utils/constance');
+const createHttpError = require('http-errors');
 const createBlogSchema = Joi.object({
-    title : Joi.string().min(3).max(30).error(new Error('the title of category not accepted')) ,
-    text : Joi.string().error(new Error('text you send is not accepted')) ,
-    short_text : Joi.string().error(new Error('text you send is not accepted')) ,
-    image : Joi.string().error(new Error('image you send is not accepted')) ,
-    tags : Joi.array().min(0).max(20).error(new Error('tags should not over 20 character')) ,
-    category : Joi.string().pattern(MongoIDPattern).error(new Error('category not found'))
+    title : Joi.string().min(3).max(30).error(createHttpError.BadRequest('the title of category not accepted')) ,
+    text : Joi.string().error(createHttpError.BadRequest('text you send is not accepted')) ,
+    short_text : Joi.string().error(createHttpError.BadRequest('text you send is not accepted')) ,
+    filename : Joi.string().pattern(/(\.png|\.jpg|\.webp|\.jpeg|\.gif)$/).error(createHttpError.BadRequest('image you send is not accepted')) ,
+    tags : Joi.array().min(0).max(20).error(createHttpError.BadRequest('tags should not over 20 character')) ,
+    category : Joi.string().pattern(MongoIDPattern).error(createHttpError.BadRequest('category not found')) ,
+    fileUploadPath : Joi.allow()
 });
 
-const updateCategorySchema = Joi.object({
-    title : Joi.string().min(3).max(30).error(new Error('the title of category not accepted'))
-})
 
 module.exports = {
     createBlogSchema

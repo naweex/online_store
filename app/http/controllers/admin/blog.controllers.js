8d641421f16a31913db98ea9b2e09ed8eaee9f18
navid bahrami <navidbahrami1996@gmail.com>
2024-10-22 12:11:52 +0330

    some changes

--- app/http/controllers/admin/blog.controllers.js
+++ app/http/controllers/admin/blog.controllers.js
@@ -96,22 +96,50 @@ class BlogController extends Controller {
     }
     async deleteBlogById(req , res , next){
         try {
-            
+            const {id} = req.params;
+            await this.findBlog(id)
+            const result = await BlogModel.deleteOne({_id : id});
+            if(result.deletedCount == 0) throw createHttpError.InternalServerError('delete operation is not successful')
+                return res.status(200).json({
+                    data: {
+                        statusCode : 200 ,
+                        message : 'blog deleted successfully'
+                    }
+            })
         } catch (error) {
             next(error)
         }
     }
     async updateBlogById(req , res , next){
         try {
+            const {id} = req.params;
+            await this.findBlog(id)
+            const author = req.user._id
+            req.body.image = path.join(req.Body.fileUploadPath , req.Body.filename)
+            req.body.image = req.body.image.replace(/\\/g , '/')
+            if (req.body.image){
+
+            }
+            const {title , text , short_text , category , tags} = blogDataBody;
+            const image  = req.body.image
+            const blog = await BlogModel.updateOne({title , image , text , short_text , category , tags , author})
+            return res.status(201).json({
+                data : {
+                    statusCode : 201 ,
+                    message : 'blog created successfully'
+                }
+            })
             
         } catch (error) {
+            deleteFileInPublic(req.body.image)
             next(error)
         }
     }
     async findBlog(query = {}){
-        const blog = await BlogModel.findOne(query).populate([{path : 'category' , select : {'children' : 0}} , {path : 'user'}]);
-        if(!blog) throw createHttpError.NotFound('blog not found')
-            return blog
+        const blog = await BlogModel.findOne(query).populate([{path : 'category' , select : ['title']} , {path : 'author' , select :['mobile' , 'first_name' , 'last_name' , 'username']}]);
+        if(!blog) throw createHttpError.NotFound('blog not found');
+        delete blog.category.children
+        return blog
     }
 }
 

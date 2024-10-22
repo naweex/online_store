40456a7366728c568252dced7b25630f6d7c7226
navid bahrami <navidbahrami1996@gmail.com>
2024-10-22 12:12:21 +0330

    some changes

--- app/http/validators/user/auth.schema.js
+++ app/http/validators/user/auth.schema.js
@@ -1,10 +1,14 @@
 const Joi = require('joi');
 
-const authSchema = Joi.object({
-    mobile : Joi.string().length(11).pattern(/^09[0-9]{9}$/), 
-    //password : Joi.string().min(6).max(16).trim().required(),
+const getOtpSchema= Joi.object({
+    mobile : Joi.string().length(11).pattern(/^09[0-9]{9}$/).error(new Error('mobile number is wrong'))
+})
+const checkOtpSchema = Joi.object({
+    mobile : Joi.string().length(11).pattern(/^09[0-9]{9}$/).error(new Error('mobile number is wrong')) ,
+    code : Joi.string().min(4).max(6).error(new Error('code is wrong'))
 })
 
 module.exports = {
-    authSchema
+    getOtpSchema ,
+    checkOtpSchema
 }

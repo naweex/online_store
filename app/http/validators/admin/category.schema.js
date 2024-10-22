531aabcb4f89c2eb2ea44151bf8b8c62e614e75b
navid bahrami <navidbahrami1996@gmail.com>
2024-10-22 12:12:04 +0330

    some changes

--- app/http/validators/admin/category.schema.js
+++ app/http/validators/admin/category.schema.js
@@ -4,7 +4,11 @@ const addCategorySchema= Joi.object({
     title : Joi.string().min(3).max(30).error(new Error('category title is not correct')) ,
     parent : Joi.string().allow('').pattern(MongoIdPattern).allow('').error(new Error('parent of chosen category not found'))
 })
+const updateCategorySchema= Joi.object({
+    title : Joi.string().min(3).max(30).error(new Error('category title is not correct'))
+})
 
 module.exports = {
-    addCategorySchema
+    addCategorySchema ,
+    updateCategorySchema
 }

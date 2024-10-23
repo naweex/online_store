30078357c377d47779487774e64b3e60fa211831
navid bahrami <navidbahrami1996@gmail.com>
2024-10-23 11:27:21 +0330

    some changes

--- app/models/blogs.js
+++ app/models/blogs.js
@@ -1,17 +1,40 @@
+const { ref, required, string } = require("joi");
 const { default: mongoose } = require("mongoose");
-
+const commentSchema = new mongoose.Schema({
+    user : {type : mongoose.Types.ObjectId , ref : 'user' , required : true} ,
+    comment : {type : String , required : true} ,
+    createdAt : {type : Date , default : new Date().getTime()} ,
+    parent : {type : mongoose.Types.ObjectId}
+})
 const Schema = new mongoose.Schema({
-    author : {type : mongoose.Types.ObjectId , required : true} ,
+    author : {type : mongoose.Types.ObjectId ,ref : 'user' , required : true} ,
     title : {type : String , required : true} ,
+    short_text : {type : String , required : true} ,
     text : {type : String , required : true} ,
     image : {type : String , required : true} ,
     tags : {type : [String] , default : []} ,
-    category : {type : mongoose.Types.ObjectId , required : true} ,
-    comment : {type : [] , default : []} ,
-    like : {type : [mongoose.Types.ObjectId] , default : []} ,
-    dislike : {type : [mongoose.Types.ObjectId] , default : []} ,
-    bookmark : {type : [mongoose.Types.ObjectId] , default : []} ,
-})
+    category : {type : [mongoose.Types.ObjectId] , required : true} ,
+    comments : {type : [commentSchema] , default : []} ,
+    likes : {type : [mongoose.Types.ObjectId] ,ref: 'users', default : []} ,
+    dislikes : {type : [mongoose.Types.ObjectId] ,ref: 'users', default : []} ,
+    bookmarks : {type : [mongoose.Types.ObjectId] ,ref: 'users', default : []} ,
+}, {
+    timestamps : true ,
+    versionKey : false ,
+    toJSON : {
+        virtuals : true
+    }
+});
+Schema.virtual('user' , {
+    ref : 'user' ,
+    localField : '_id' ,
+    foreignField : 'author'
+});
+Schema.virtual('category_detail' , {
+    ref : 'category' ,
+    localField : '_id' ,
+    foreignField : 'category'
+});
 
 module.exports = {
     BlogModel : mongoose.model('blog' , Schema)

21faf0d05d30e823490fa7bd8f75462d2602a437
navid bahrami <navidbahrami1996@gmail.com>
2024-10-24 13:55:17 +0330

    some changes

--- app/models/products.js
+++ app/models/products.js
@@ -1,23 +1,23 @@
 const { default: mongoose } = require("mongoose");
+const { commentSchema } = require("./public.schema");
 
 const Schema = new mongoose.Schema({
     title : {type : String , required : true} ,
-    short_desc : {type : String , required : true} ,
-    long_desc : {type : String , required : true} ,
+    short_text : {type : String , required : true} ,
+    text: {type : String , required : true} ,
     images : {type : [String] , required : true} ,
     tags : {type : [String] , default : []} ,
-    category : {type : mongoose.Types.ObjectId , required : true} ,
-    comments : {type : [] , default : []} ,
-    like : {type : [mongoose.Types.ObjectId] , default : []} ,
-    dislike : {type : [mongoose.Types.ObjectId] , default : []} ,
-    bookmark : {type : [mongoose.Types.ObjectId] , default : []} ,
+    category : {type : mongoose.Types.ObjectId ,ref : 'category', required : true} ,
+    comments : {type : [commentSchema] , default : []} ,
+    likes : {type : [mongoose.Types.ObjectId] , default : []} ,
+    dislikes : {type : [mongoose.Types.ObjectId] , default : []} ,
+    bookmarks : {type : [mongoose.Types.ObjectId] , default : []} ,
     price : {type : Number , default : 0} ,
     discount : {type : Number , default : 0 } ,
     count : {type : Number } ,
     type : {type : String , required : true} ,
-    time : {type : String } ,
     format : {type : String } ,
-    teacher : {type : mongoose.Types.ObjectId, required : true} ,
+    supplier : {type : mongoose.Types.ObjectId, required : true} ,
     feature : {type : Object , default : {
         length : '' ,
         heigh : '' ,

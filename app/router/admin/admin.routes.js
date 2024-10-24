e960d36603c0fecb3ae82fcbe17c154449e36bf0
navid bahrami <navidbahrami1996@gmail.com>
2024-10-24 13:55:57 +0330

    add product route

---
+++ app/router/admin/admin.routes.js
@@ -0,0 +1,23 @@
+const { verifyAccessToken } = require('../../http/middlewares/verifyAccessToken')
+const { AdminApiBlogRouter } = require('./blog')
+const { AdminApiCategoryRouter } = require('./category')
+const { AdminApiProductRouter } = require('./product')
+const router = require('express').Router()
+/**
+ * @swagger
+ *  tags:
+ *      -   name: Admin-Panel
+ *          description: action of Admin includes(add,delete,update,etc...)
+ *      -   name: Category(AdminPanel)
+ *          description: all method and routes about category section
+ *      -   name: Blog(AdminPanel)
+ *          description: make blog management admin panel
+ *      
+ */
+
+router.use('/category' , AdminApiCategoryRouter)
+router.use('/blogs' ,AdminApiBlogRouter )
+router.use('/product' , AdminApiProductRouter)
+module.exports = {
+    AdminRoutes : router
+}

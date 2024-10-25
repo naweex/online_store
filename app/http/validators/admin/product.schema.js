4b9e235892c3d3592b7e0652b0d7e2a59e0e701a
navid bahrami <navidbahrami1996@gmail.com>
2024-10-25 18:29:04 +0330

    some changes

--- app/http/validators/admin/product.schema.js
+++ app/http/validators/admin/product.schema.js
@@ -6,7 +6,7 @@ const createProductSchema = Joi.object({
     text : Joi.string().error(createHttpError.BadRequest('text you send is not accepted')) ,
     short_text : Joi.string().error(createHttpError.BadRequest('text you send is not accepted')) ,
     tags : Joi.array().min(0).max(20).error(createHttpError.BadRequest('tags should not over 20 character')) ,
-    category : Joi.string().pattern(MongoIDPattern).error(createHttpError.BadRequest('category not found')) ,
+    category : Joi.string().regex(MongoIDPattern).error(createHttpError.BadRequest('category not found')) ,
     price : Joi.number().error(createHttpError.BadRequest('input price is not valid')) ,
     discount : Joi.number().error(createHttpError.BadRequest('discount is not valid')) ,
     count : Joi.number().error(createHttpError.BadRequest('number of product is not valid')) ,
@@ -14,7 +14,8 @@ const createProductSchema = Joi.object({
     length : Joi.number().allow(null , 0 , '0').error(createHttpError.BadRequest('length of product is not valid')) ,
     height : Joi.number().allow(null , 0 , '0').error(createHttpError.BadRequest('height of product is not valid')) ,
     width : Joi.number().allow(null , 0 , '0').error(createHttpError.BadRequest('width of product is not valid')) ,
-    filename : Joi.string().pattern(/(\.png|\.jpg|\.webp|\.jpeg|\.gif)$/).error(createHttpError.BadRequest('image you send is not accepted')) ,
+    type : Joi.string().regex(/(virtual|physical)/i) ,
+    filename : Joi.string().regex(/(\.png|\.jpg|\.webp|\.jpeg|\.gif)$/).error(createHttpError.BadRequest('image you send is not accepted')) ,
     fileUploadPath : Joi.allow()
 });
 

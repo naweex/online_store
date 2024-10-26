620f7940fb76ab021001c3fdd3c5f0338b48079f
navid bahrami <navidbahrami1996@gmail.com>
2024-10-26 12:07:57 +0330

    some changes

--- app/router/admin/category.js
+++ app/router/admin/category.js
@@ -5,7 +5,7 @@ const router = require('express').Router()
  * @swagger
  *  /admin/category/add:
  *      post:
- *          tags: [Admin-Panel]
+ *          tags: [Category(AdminPanel)]
  *          summary: create new category title
  *          parameters:
  *              -   in: formData
@@ -26,7 +26,7 @@ router.post('/add' , CategoryController.addCategory)
  * @swagger
  *  /admin/category/parents:
  *      get:
- *          tags: [Admin-Panel]
+ *          tags: [Category(AdminPanel)]
  *          summary: get all parents of Category
  *          responses:
  *              200:
@@ -40,7 +40,7 @@ router.get('/parents' , CategoryController.getAllParents)
  * @swagger
  *  /admin/category/Children/{parent}:
  *      get:
- *          tags: [Admin-Panel]
+ *          tags: [Category(AdminPanel)]
  *          summary: get all children of parents Category
  *          parameters:
  *              -   in: path
@@ -59,7 +59,7 @@ router.get('/children/:parent' , CategoryController.getChildOfParents)
  * @swagger
  *  /admin/category/all:
  *      get:
- *          tags: [Admin-Panel]
+ *          tags: [Category(AdminPanel)]
  *          summary: get all Category
  *          responses:
  *              200:
@@ -73,7 +73,7 @@ router.get('/all' , CategoryController.getAllCategory)
  * @swagger
  *  /admin/category/remove/{id}:
  *      delete:
- *          tags: [Admin-Panel]
+ *          tags: [Category(AdminPanel)]
  *          summary: remove Category with Object-id
  *          parameters:
  *              -   in: path
@@ -85,8 +85,65 @@ router.get('/all' , CategoryController.getAllCategory)
  *                  description: success 
  */
 
-
 router.delete('/remove/:id' , CategoryController.removeCategory)
+
+/**
+ * @swagger
+ *  /admin/category/list-of-all:
+ *      get:
+ *          tags: [Category(AdminPanel)]
+ *          summary: get all categories without populate and nested structure
+ *          responses:
+ *              200:
+ *                  description: success 
+ */
+
+router.get('/list-of-all' , CategoryController.getAllCategoryWithOutPopulate)
+
+/**
+ * @swagger
+ *  /admin/category/{id}:
+ *      get:
+ *          tags: [Category(AdminPanel)]
+ *          summary: find and get Category by Object-id
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  type: string
+ *                  required: true
+ *          responses:
+ *              200:
+ *                  description: success 
+ */
+
+
+router.get('/:id' , CategoryController.getCategoryById)
+
+/**
+ * @swagger
+ *  /admin/category/update/{id}:
+ *      patch:
+ *          tags: [Category(AdminPanel)]
+ *          summary: edit or update category title with Object-id
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  type: string
+ *                  required: true
+ *              -   in: formData
+ *                  name: title
+ *                  type: string
+ *                  required: true
+ *          responses:
+ *              200:
+ *                  description: success
+ *              500:
+ *                  description: InternalServerError 
+ */
+
+
+router.patch('/update/:id' , CategoryController.editCategoryTitle)
+
 module.exports = {
-    CategoryRoutes : router
+    AdminApiCategoryRouter : router
 }

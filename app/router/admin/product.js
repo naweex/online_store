34219b50afebc5f34e46cab9e478bbeeee0b6206
navid bahrami <navidbahrami1996@gmail.com>
2024-10-26 12:11:03 +0330

    add delete route

--- app/router/admin/product.js
+++ app/router/admin/product.js
@@ -6,8 +6,8 @@ const router = require('express').Router();
 router.post('/add' , uploadFile.array('images' , 10),stringToArray('tags') , ProductController.addProduct)
 router.get('/list' , ProductController.getAllProducts)
 router.get('/:id' , ProductController.getOneProduct)
+router.delete('/remove/:id' , ProductController.removeProduct)
 router.patch()
-router.delete()
 module.exports = {
     AdminApiProductRouter : router
 }

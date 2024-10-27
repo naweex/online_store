95ff62bfd0e08f9b213c9616806916759ef6b007
navid bahrami <navidbahrami1996@gmail.com>
2024-10-27 17:04:24 +0330

    create multer

---
+++ app/utils/multer.js
@@ -0,0 +1,45 @@
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs');
+const createHttpError = require('http-errors');
+function createRoutes (req){
+    const date = new Date();
+    const year = date.getFullYear().toString();
+    const month = date.getMonth().toString();
+    const day = date.getDate().toString();
+    const directory = path.join(__dirname , '..' , '..' , 'public' , 'uploads' , 'blogs' , year , month , day)
+    req.body.fileUploadPath = path.join('uploads' , 'blogs' , year , month , day)
+    fs.mkdirSync(directory , {recursive : true})
+    return directory
+}
+const storage = multer.diskStorage({
+    destination : (req , file , cb) => {
+        if(file?.originalname){
+        const filePath = createRoutes(req);
+        return cb(null , filePath)
+        }
+        cb(null , null)
+    },
+    filename : (req , file , cb) => {
+    if(file?.originalname){
+        const ext = path.extname(file.originalname);
+        const fileName = String(new Date().getTime() + ext);
+        req.body.filename = fileName
+        return cb(null , fileName)
+    }
+        cb(null , null)
+    }
+});
+function fileFilter(req , file , cb){
+    const ext = path.extname(file.originalname);
+    const mimeTypes = ['.jpg' , ',jpeg' , '.webp' , '.gif' , '.png']
+    if(mimeTypes.includes(ext)){
+        return cb(null , true)
+    }
+    return cb(createHttpError.BadRequest('format of file not accepted'))
+}
+const maxSize = 1 * 1000 * 1000
+const uploadFile = multer({storage ,fileFilter, limits: {fileSize : maxSize}})
+module.exports = {
+    uploadFile
+}

648c96e3ed5d95a01aebf851e889b4860064bf40
navid bahrami <navidbahrami1996@gmail.com>
2024-10-28 13:44:35 +0330

    some changes

--- app/http/validators/admin/product.schema.js
+++ app/http/validators/admin/product.schema.js
@@ -2,12 +2,12 @@ const Joi = require('joi')
 const { MongoIDPattern } = require('../../../utils/constance');
 const createHttpError = require('http-errors');
 const createProductSchema = Joi.object({
-    title : Joi.string().min(3).max(30).error(createHttpError.BadRequest('the title of category not accepted')) ,
+    title : Joi.string().min(3).max(30).error(createHttpError.BadRequest('the title of product not accepted')) ,
     text : Joi.string().error(createHttpError.BadRequest('text you send is not accepted')) ,
     short_text : Joi.string().error(createHttpError.BadRequest('text you send is not accepted')) ,
     tags : Joi.array().min(0).max(20).error(createHttpError.BadRequest('tags should not over 20 character')) ,
     colors : Joi.array().min(0).max(20).error(createHttpError.BadRequest('tags should not over 20 character')) ,
-    category : Joi.string().regex(MongoIDPattern).error(createHttpError.BadRequest('category not found')) ,
+    category : Joi.string().regex(MongoIDPattern).error(createHttpError.BadRequest('product not found')) ,
     price : Joi.number().error(createHttpError.BadRequest('input price is not valid')) ,
     discount : Joi.number().error(createHttpError.BadRequest('discount is not valid')) ,
     count : Joi.number().error(createHttpError.BadRequest('number of product is not valid')) ,

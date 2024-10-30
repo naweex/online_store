a33e619424a8c1eafc7797f1adb0aff4c3818910
navid bahrami <navidbahrami1996@gmail.com>
2024-10-30 12:45:48 +0330

    some changes

---
+++ app/http/controllers/admin/course/course.controller.js
@@ -0,0 +1,117 @@
+const createHttpError = require('http-errors');
+const { CourseModel } = require('../../../../models/course');
+const {
+  createCourseSchema,
+} = require('../../../validators/admin/course.schema');
+const Controller = require('../../controller');
+const { StatusCodes: HttpStatus } = require('http-status-codes');
+const path = require('path');
+const { default: mongoose } = require('mongoose');
+class CourseController extends Controller {
+  async getListOfCourse(req, res, next) {
+    try {
+      const { search } = req.query;
+      let courses;
+      if (search)
+        courses = await CourseModel.find({ $text: { $search: search } }).sort({
+          _id: -1,
+        });
+      else courses = await CourseModel.find({}).sort({ _id: -1 });
+      return res.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        data: {
+          courses,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async addCourse(req, res, next) {
+    try {
+      await createCourseSchema.validateAsync(req.body);
+      const { fileUploadPath, filename } = req.body;
+      const image = path.join(fileUploadPath, filename).replace(/\\/g, '/');
+      const { title, short_text, text, tags, category, price, discount, type } =
+        req.body;
+      const teacher = req.user._id;
+      if (Number(price) > 0 && type === 'free')
+        throw createHttpError.BadRequest(
+          'for free courses do not need any price'
+        );
+      const course = CourseModel.create({
+        title,
+        short_text,
+        text,
+        tags,
+        category,
+        price,
+        discount,
+        type,
+        image: '00:00:00',
+        status: 'notStarted',
+        teacher,
+      });
+      if (!course)
+        throw createHttpError.InternalServerError('course not registered');
+      return res.status(HttpStatus.CREATED).json({
+        statusCode: HttpStatus.CREATED,
+        data: {
+          message: 'course created successfully',
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async getCourseById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const course = await CourseModel.findById(id);
+      if (!course) throw createHttpError.NotFound('not found any course');
+      return res.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        data: {
+          course,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async addChapter(req, res, next) {
+    try {
+      const { id, title, text } = req.body;
+      await this.findCourseById(id);
+      const saveChapterResult = await CourseModel.updateOne(
+        { _id: id },
+        {
+          $push: {
+            chapters: { title, text, episodes: [] },
+          },
+        }
+      );
+      if (saveChapterResult.modifiedCount == 0)
+        throw createHttpError.InternalServerError('chapter is not added');
+      return res.status(HttpStatus.CREATED).json({
+        statusCode: HttpStatus.CREATED,
+        data: {
+          message: 'chapter added successfully',
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async findCourseById(id) {
+    if (!mongoose.isValidObjectId(id))
+      throw createHttpError.BadRequest('id is not valid');
+    const course = await CourseModel.findById(id);
+    if (!course) throw createHttpError.NotFound('course not found');
+    return course;
+  }
+}
+
+module.exports = {
+  CourseController: new CourseController(),
+};

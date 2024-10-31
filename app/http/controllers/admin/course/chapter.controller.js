a549b440613b4672f5a7d572c6e80be17c6c2cd2
navid bahrami <navidbahrami1996@gmail.com>
2024-10-31 12:21:33 +0330

    add remove part

--- app/http/controllers/admin/course/chapter.controller.js
+++ app/http/controllers/admin/course/chapter.controller.js
@@ -31,23 +31,54 @@ class ChapterController extends Controller {
   }
   async ChaptersOfCourse(req, res, next) {
     try {
-      const {id} = req.params;
-      const chapters = await this.getChaptersOfCourse(id)
+      const {courseID} = req.params;
+      const course = await this.getChaptersOfCourse(courseID)
       return res.status(HttpStatus.OK).json({
         statusCode: HttpStatus.OK,
         data: {
-          chapters
+          course
         },
       });
     } catch (error) {
       next(error);
     }
   }
+  async removeChapterById(req , res , next){
+    try {
+    const {chapterID} = req.params;
+    const chapter = await this.getOneChapter(chapterID)
+    const removeChapterResult = await CourseModel.updateOne({'chapters._id' : chapterID} , {
+    $pull : {
+      chapters :{
+        _id : chapterID
+      } 
+    }
+  })
+    if(removeChapterResult.modifiedCount == 0) throw createHttpError.InternalServerError('chapter not deleted')
+      res.status(HttpStatus.OK).json({
+        statusCode : HttpStatus.OK ,
+        data : {
+          message : 'chapter deleted successfully'
+        }
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
   async getChaptersOfCourse(id){
-    const chapters = await CourseModel.findOne({_id : id} , {chapters : 1})
+    const chapters = await CourseModel.findOne({_id : id} , {chapters : 1 , title : 1})
     if(!chapters) throw createHttpError.NotFound('not found any course with this id')
+      return chapters
+  }
+  async getOneChapter(id){
+    const chapter = await CourseModel.findOne({'chapters._id' : id} , {'chapters.$' : 1})
+    if(!chapter) throw createHttpError.NotFound('not found any chapter')
+      return chapter
   }
 }
+
+
+
 module.exports = {
     ChapterController : new ChapterController()
 }

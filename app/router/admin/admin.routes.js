61b1a9c77d1e0c3e46528eceb953493b34823cce
navid bahrami <navidbahrami1996@gmail.com>
2024-11-01 17:19:51 +0330

    main routes

--- app/router/admin/admin.routes.js
+++ app/router/admin/admin.routes.js
@@ -1,29 +1,16 @@
 const { verifyAccessToken } = require('../../http/middlewares/verifyAccessToken')
 const { AdminApiBlogRouter } = require('./blog')
 const { AdminApiCategoryRouter } = require('./category')
+const { AdminApiChapterRouter } = require('./chapter')
 const { AdminApiCourseRouter } = require('./course')
 const { AdminApiProductRouter } = require('./product')
 const router = require('express').Router()
-/**
- * @swagger
- *  tags:
- *      -   name: Admin-Panel
- *          description: action of Admin includes(add,delete,update,etc...)
- *      -   name: Course(AdminPanel)
- *          description: management course section like manage episode , chapter and courses
- *      -   name: Product(AdminPanel)
- *          description: management product routes 
- *      -   name: Category(AdminPanel)
- *          description: all method and routes about category section
- *      -   name: Blog(AdminPanel)
- *          description: make blog management admin panel
- *      
- */
 
 router.use('/category' , AdminApiCategoryRouter)
 router.use('/blogs' ,AdminApiBlogRouter )
 router.use('/products' , AdminApiProductRouter)
 router.use('/courses' , AdminApiCourseRouter)
+router.use('/chapter' , AdminApiChapterRouter)
 module.exports = {
     AdminRoutes : router
 }

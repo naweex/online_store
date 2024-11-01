4628f23f35388f596ff3e179f599d906ced11e69
navid bahrami <navidbahrami1996@gmail.com>
2024-11-01 17:21:03 +0330

    complete course routes

--- app/router/admin/course.js
+++ app/router/admin/course.js
@@ -1,17 +1,24 @@
-const courseController = require('../../http/controllers/admin/course.controller');
-const { CourseController } = require('../../http/controllers/admin/course.controller');
+
+const {
+  CourseController,
+} = require('../../http/controllers/admin/course/course.controller');
 const { stringToArray } = require('../../http/middlewares/stringToArray');
-const { uploadFile } = require('../../utils/multer')
+const { uploadFile } = require('../../utils/multer');
 const router = require('express').Router();
 
-router.get('/list' , CourseController.getListOfCourse)
-router.post('/add' , uploadFile.single('image'),stringToArray('tags'), CourseController.addCourse)
-router.get('/:id' , CourseController.getCourseById)
-router.patch()
-router.delete()
-router.put()
-router.put()
+router.get('/list', CourseController.getListOfCourse);
+router.post(
+  '/add',
+  uploadFile.single('image'),
+  stringToArray('tags'),
+  CourseController.addCourse
+);
+router.get('/:id', CourseController.getCourseById);
+
+router.patch();
+router.delete();
+router.put();
 
 module.exports = {
-    AdminApiCourseRouter : router
-}
+  AdminApiCourseRouter: router,
+};

0eedef19c004acb4e56e63753e336f6fe69bf1a4
navid bahrami <navidbahrami1996@gmail.com>
2024-11-02 11:51:52 +0330

    clean code

--- app/http/validators/public.validator.js
+++ app/http/validators/public.validator.js
@@ -1,6 +1,7 @@
 const Joi = require('joi')
 const { MongoIdPattern } = require('../../utils/constance')
 const createHttpError = require('http-errors')
+
 const objectIdValidators = Joi.object({
     id : Joi.string().pattern(MongoIdPattern).error(new Error(createHttpError.BadRequest('id is not valid')))
 })

ee34f1d9c50e2f8ffa4f7c9aa9ea8f50062c9ba9
navid bahrami <navidbahrami1996@gmail.com>
2024-11-05 13:01:22 +0330

    create permission model

---
+++ app/models/permissions.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose");
+
+const PermissionSchema = new mongoose.Schema({
+    title : {type : String , unique : true} ,
+    description : {type : String , default : ''} ,
+} , {
+    toJSON : {virtuals : true}
+})
+
+module.exports = {
+    PermissionModel : mongoose.model('permission' , PermissionSchema)
+}

eec0beee212ccb3014d40519b91ce766de3dc817
navid bahrami <navidbahrami1996@gmail.com>
2024-11-09 11:11:04 +0330

    episode routers

---
+++ app/router/admin/episode.js
@@ -0,0 +1,9 @@
+const { EpisodeController } = require('../../http/controllers/admin/course/episode.controller')
+const {uploadVideo} = require('../../utils/multer')
+const router = require('express').Router()
+router.post('/add' ,uploadVideo.single('video'), EpisodeController.addNewEpisode)
+router.delete('/remove/:episodeID' , EpisodeController.removeEpisode)
+router.patch('/update/:episodeID' , uploadVideo.single('video') , EpisodeController.updateEpisode)
+module.exports = {
+    AdminApiEpisodeRouter : router
+}

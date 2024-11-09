65866b29f9e657ab9bc6baa8b7f5293adff121c1
navid bahrami <navidbahrami1996@gmail.com>
2024-11-09 11:10:37 +0330

    routes

--- app/router/router.js
+++ app/router/router.js
@@ -1,3 +1,5 @@
+const { graphql } = require('graphql')
+const { createHandler ,createClient } =require('graphql-http')
 const { verifyAccessToken, checkRole } = require('../http/middlewares/verifyAccessToken')
 const { AdminRoutes } = require('./admin/admin.routes')
 const { homeRoutes } = require('./api')
@@ -8,6 +10,7 @@ const router = require('express').Router()
 router.use('/user' , UserAuthRoutes)
 router.use('/admin' , verifyAccessToken , AdminRoutes)
 router.use('/developer' , DeveloperRoutes)
+router.use('/graphql' , )
 router.use('/' , homeRoutes)
 module.exports = {
     AllRoutes : router

dd972b9269a685ab7c5d1c31224a23127684e79d
navid bahrami <navidbahrami1996@gmail.com>
2024-11-19 18:20:28 +0330

    course schema

--- app/http/validators/admin/course.schema.js
+++ app/http/validators/admin/course.schema.js
@@ -9,17 +9,17 @@ const createCourseSchema = Joi.object({
     category : Joi.string().regex(MongoIDPattern).error(createHttpError.BadRequest('course not found')) ,
     price : Joi.number().error(createHttpError.BadRequest('input price is not valid')) ,
     discount : Joi.number().error(createHttpError.BadRequest('discount is not valid')) ,
-    type : Joi.string().regex(/(free|cash|special)/i) ,
+    type : Joi.string().RegExp(/(free|cash|special)/i) ,
     filename : Joi.string().regex(/(\.mp4|\.mov|\.avi|\.mkv|\.mpg)$/).error(createHttpError.BadRequest('your uploaded video is not accepted')) ,
     fileUploadPath : Joi.allow()
 });
 const createEpisodeSchema = Joi.object({
     title : Joi.string().min(3).max(30).error(createHttpError.BadRequest('the title of course not accepted')) ,
     text : Joi.string().error(createHttpError.BadRequest('text you send is not accepted')) ,
-    type : Joi.string().regex(/(lock|unlock)/i) ,
-    chapterID : Joi.string().regex(MongoIDPattern).error(createHttpError.BadRequest('chapter ID is invalid')) ,
-    courseID : Joi.string().regex(MongoIDPattern).error(createHttpError.BadRequest('course ID is invalid')) ,
-    filename : Joi.string().regex(/(\.png|\.jpg|\.webp|\.jpeg|\.gif)$/).error(createHttpError.BadRequest('image you send is not accepted')) ,
+    type : Joi.string().RegExp(/(lock|unlock)/i) ,
+    chapterID : Joi.string().RegExp(MongoIDPattern).error(createHttpError.BadRequest('chapter ID is invalid')) ,
+    courseID : Joi.string().RegExp(MongoIDPattern).error(createHttpError.BadRequest('course ID is invalid')) ,
+    filename : Joi.string().RegExp(/(\.png|\.jpg|\.webp|\.jpeg|\.gif)$/).error(createHttpError.BadRequest('image you send is not accepted')) ,
     fileUploadPath : Joi.allow()
 });
 

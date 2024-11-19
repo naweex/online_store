9e5549f076caeebe09b11c8cf612d6209bfbbd9b
navid bahrami <navidbahrami1996@gmail.com>
2024-11-19 18:20:08 +0330

    category schema

--- app/http/validators/admin/category.schema.js
+++ app/http/validators/admin/category.schema.js
@@ -1,8 +1,9 @@
 const Joi = require('joi');
+const { MongoIDPattern } = require('../../../utils/constance');
 
 const addCategorySchema= Joi.object({
     title : Joi.string().min(3).max(30).error(new Error('category title is not correct')) ,
-    parent : Joi.string().allow('').pattern(MongoIdPattern).allow('').error(new Error('parent of chosen category not found'))
+    parent : Joi.string().allow('').pattern(MongoIDPattern).allow('').error(new Error('parent of chosen category not found'))
 })
 const updateCategorySchema= Joi.object({
     title : Joi.string().min(3).max(30).error(new Error('category title is not correct'))

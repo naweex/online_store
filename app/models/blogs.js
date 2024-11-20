84f09777b7ee5863e93c7c95b6e88b1f222c4378
navid bahrami <navidbahrami1996@gmail.com>
2024-11-20 17:30:28 +0330

    blog models

--- app/models/blogs.js
+++ app/models/blogs.js
@@ -1,42 +1,37 @@
-const { ref, required, string } = require("joi");
 const { default: mongoose } = require("mongoose");
-const { commentSchema } = require("./public.schema");
-
+const { CommentSchema } = require("./public.schema");
 const BlogSchema = new mongoose.Schema({
-    author : {type : mongoose.Types.ObjectId ,ref : 'user' , required : true} ,
-    title : {type : String , required : true} ,
-    short_text : {type : String , required : true} ,
-    text : {type : String , required : true} ,
-    image : {type : String , required : true} ,
-    tags : {type : [String] , default : []} ,
-    category : {type : [mongoose.Types.ObjectId] , required : true} ,
-    comments : {type : [commentSchema] , default : []} ,
-    likes : {type : [mongoose.Types.ObjectId] ,ref: 'users', default : []} ,
-    dislikes : {type : [mongoose.Types.ObjectId] ,ref: 'users', default : []} ,
-    bookmarks : {type : [mongoose.Types.ObjectId] ,ref: 'users', default : []} ,
+    author : {type : mongoose.Types.ObjectId, ref: "user", required : true},
+    title : {type : String, required : true},
+    short_text : {type : String, required : true},
+    text : {type : String, required : true},
+    image : {type : String, required : true},
+    tags : {type : [String], default : []},
+    category : {type : mongoose.Types.ObjectId, ref: "category", required :true},
+    comments : {type : [CommentSchema], default : []},
+    likes : {type : [mongoose.Types.ObjectId], ref: "user", default : []},
+    dislikes : {type : [mongoose.Types.ObjectId], ref: "user", default : []},
+    bookmarks : {type : [mongoose.Types.ObjectId], ref: "user", default : []}
 }, {
-    timestamps : true ,
-    versionKey : false ,
+    timestamps : true, 
+    versionKey : false,
     toJSON : {
-        virtuals : true
+        virtuals: true
     }
 });
-BlogSchema.virtual('user' , {
-    ref : 'user' ,
-    localField : '_id' ,
-    foreignField : 'author'
-});
-BlogSchema.virtual('category_detail' , {
-    ref : 'category' ,
-    localField : '_id' ,
-    foreignField : 'category'
-});
-
-BlogSchema.virtual('imageURL').get(function(){
+BlogSchema.virtual("user", {
+    ref : "user",
+    localField : "_id",
+    foreignField: "author"
+})
+BlogSchema.virtual("category_detail", {
+    ref : "category",
+    localField : "_id",
+    foreignField: "category"
+})
+BlogSchema.virtual("imageURL").get(function(){
     return `${process.env.BASE_URL}:${process.env.APPLICATION_PORT}/${this.image}`
 })
-
-
 module.exports = {
-    BlogModel : mongoose.model('blog' , BlogSchema)
+    BlogModel : mongoose.model("blog", BlogSchema)
 }

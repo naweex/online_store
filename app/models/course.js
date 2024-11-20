932e776c5b763af3c5dab44c94883619198512b4
navid bahrami <navidbahrami1996@gmail.com>
2024-11-20 17:31:26 +0330

    course models

--- app/models/course.js
+++ app/models/course.js
@@ -1,55 +1,54 @@
 const { default: mongoose } = require("mongoose");
-const { commentSchema } = require("./public.schema");
-const { ref, string } = require("joi");
-const { text } = require("body-parser");
 const { getTimeOfCourse } = require("../utils/functions");
-const Episode = new mongoose.Schema({
-    title : {type : String , required : true} ,
-    text : {type : String , required : true} ,
-    type : {type : String , default : 'free'} ,
-    time : {type : String , required : true} ,
-    videoAddress : {type : String , require : true}
-} , {toJSON : {virtuals : true}})
-Episode.virtual('videoURL').get(function(){
+const { CommentSchema } = require("./public.schema");
+const Episodes = new mongoose.Schema({
+    title: {type: String, required: true},
+    text: {type: String, required: true},
+    type: {type: String, default: "unlock"},
+    time: {type: String, required : true}, 
+    videoAddress: {type: String, required: true}
+}, {toJSON: {virtuals: true}})
+Episodes.virtual("videoURL").get(function(){
     return `${process.env.BASE_URL}:${process.env.APPLICATION_PORT}/${this.videoAddress}`
 })
 const Chapter = new mongoose.Schema({
-    title : {type : String , required : true} ,
-    text : {type : String , default : ''} ,
-    episodes : {type : [Episode] , default : []} , 
+    title : {type: String, required : true},
+    text: {type: String, default : ""},
+    episodes : {type: [Episodes], default : []}
 })
-const courseSchema = new mongoose.Schema({
-    title : {type : String , required : true} ,
-    short_text : {type : String , required : true} ,
-    text : {type : String , required : true} ,
-    image : {type : String , required : true} ,
-    tags : {type : [String] , default : []} ,
-    category : {type : mongoose.Types.ObjectId ,ref : 'category', required : true} ,
-    comments : {type : [commentSchema] , default : []} ,
-    likes : {type : [mongoose.Types.ObjectId] , default : []} ,
-    dislikes : {type : [mongoose.Types.ObjectId] , default : []} ,
-    bookmarks : {type : [mongoose.Types.ObjectId] , default : []} ,
-    price : {type : Number , default : 0} ,
-    discount : {type : Number , default : 0 } ,
-    type : {type : String , default : 'free'/*free , cash , special */,required : true} ,
-    status : {type : String , default : 'not started' ,/*notStarted,completed,opening*/} ,
-    teacher : {type : mongoose.Types.ObjectId,ref : 'user', required : true} ,
-    chapters : {type : [Chapter] , default : [] } ,
-    students : {type : [mongoose.Types.ObjectId] , default : [] ,ref: 'user' }
-},{
-    toJSON : {
-        virtuals : true
+
+const CourseSchema = new mongoose.Schema({
+    title : {type: String, required : true},
+    short_text : {type: String, required : true},
+    text : {type: String, required : true},
+    image : {type: String, required : true},
+    tags : {type: [String], default : []},
+    category : {type: mongoose.Types.ObjectId, ref: "category", required : true},
+    comments : {type: [CommentSchema], default : []},
+    likes : {type: [mongoose.Types.ObjectId], ref: "user", default : []},
+    dislikes : {type: [mongoose.Types.ObjectId], ref: "user", default : []},
+    bookmarks : {type: [mongoose.Types.ObjectId], ref: "user", default : []},
+    price : {type: Number, default : 0},
+    discount : {type: Number, default : 0},
+    type : {type: String, default: "free"/*free, cash, special */, required : true},
+    status: {type: String, default: "notStarted" /*notStarted, Completed, Holding*/},
+    teacher : {type: mongoose.Types.ObjectId, ref: "user", required : true},
+    chapters : {type: [Chapter], default: []},
+    students : {type : [mongoose.Types.ObjectId], default : [], ref: "user"}
+}, {
+    toJSON: {
+        virtuals: true
     }
 });
-courseSchema.index({title : 'text' , short_text : 'text' , text : 'text'})
+CourseSchema.index({title: "text", short_text: "text", text : "text"})
 
-courseSchema.virtual('imageURL').get(function(){
+CourseSchema.virtual("imageURL").get(function(){
     return `${process.env.BASE_URL}:${process.env.APPLICATION_PORT}/${this.image}`
 })
-courseSchema.virtual('totalTime').get(function(){
+CourseSchema.virtual("totalTime").get(function(){
     return getTimeOfCourse(this.chapters || [])
 })
 
 module.exports = {
-    CourseModel : mongoose.model('course' , courseSchema)
+    CourseModel : mongoose.model("course", CourseSchema)
 }

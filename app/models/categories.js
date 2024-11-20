c87bcb17c859898b90119042b7935f560b3ebc8a
navid bahrami <navidbahrami1996@gmail.com>
2024-11-20 17:30:57 +0330

    category  models

--- app/models/categories.js
+++ app/models/categories.js
@@ -1,26 +1,24 @@
-const { ref } = require("joi");
 const { default: mongoose } = require("mongoose");
 
 const Schema = new mongoose.Schema({
-    title : {type : String , required : true} ,
-    parent : {type : mongoose.Types.ObjectId ,ref : 'category', default : undefined}
-},{
-    id : false ,
+    title : {type : String, required : true},
+    parent : {type : mongoose.Types.ObjectId, ref: "category", default : undefined}
+}, {
+    id : false,
     toJSON : {
-        virtuals : true
+        virtuals: true
     }
 });
-Schema.virtual('children' , {
-    ref : 'category' ,
-    localField : '_id' ,
-    foreignField : 'parent'
+Schema.virtual("children", {
+    ref : "category",
+    localField : "_id",
+    foreignField: "parent"
 })
-function autoPopulate(next){
-    this.populate([{path : 'children' , select : {__v : 0 , id : 0}}]);
+function autoPopulate(next) {
+    this.populate([{path : "children", select : {__v : 0, id : 0}}]);
     next()
 }
-Schema.pre('findOne' ,autoPopulate).pre('find' , autoPopulate)
-
+Schema.pre('findOne', autoPopulate).pre("find", autoPopulate)
 module.exports = {
-    CategoryModel : mongoose.model('category' , Schema)
+    CategoryModel : mongoose.model("category", Schema)
 }

c29523984e2932da237d8b4b72cb1c83ff70b304
navid bahrami <navidbahrami1996@gmail.com>
2024-11-20 18:40:08 +0330

    functions

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -1,164 +1,255 @@
-const createHttpError = require('http-errors');
-const JWT = require('jsonwebtoken');
-const { token } = require('morgan');
-const { UserModel } = require('../models/users');
-const fs = require('fs')
-const { SECRET_KEY, REFRESH_TOKEN_SECRET_KEY } = require('./constance');
-const path = require('path');
-const { array } = require('joi');
-function randomInt(){
+const JWT = require("jsonwebtoken")
+const createError = require("http-errors")
+const { UserModel } = require("../models/users")
+const fs = require("fs");
+const path = require("path");
+const moment = require("moment-jalali");
+const { ACCESS_TOKEN_SECRET_KEY, REFRESH_TOKEN_SECRET_KEY } = require("./constans");
+const redisClient = require("./initRedis");
+function RandomNumberGenerator() {
     return Math.floor((Math.random() * 90000) + 10000)
 }
-function SignAccessToken(userId){
-    return new Promise(async(resolve , reject) => {
+function SignAccessToken(userId) {
+    return new Promise(async (resolve, reject) => {
         const user = await UserModel.findById(userId)
         const payload = {
-            mobile : user.mobile
+            mobile: user.mobile
         };
         const options = {
-            expireIn : '1h'
+            expiresIn: "1d"
         };
-        JWT.sign(payload , SECRET_KEY , options ,(err,token) => {
-            if (err) (createHttpError.InternalServerError('Internal Server Error'));
+        JWT.sign(payload, ACCESS_TOKEN_SECRET_KEY, options, (err, token) => {
+            if (err) reject(createError.InternalServerError("خطای سروری"));
             resolve(token)
         })
     })
 }
-function SignRefreshToken(userId){
-    return new Promise(async(resolve , reject) => {
+function SignRefreshToken(userId) {
+    return new Promise(async (resolve, reject) => {
         const user = await UserModel.findById(userId)
         const payload = {
-            mobile : user.mobile
+            mobile: user.mobile
         };
         const options = {
-            expireIn : '1y'
+            expiresIn: "1y"
         };
-        JWT.sign(payload , REFRESH_TOKEN_SECRET_KEY , options , async(err,token) => {
-            if (err) (createHttpError.InternalServerError('Internal Server Error'));
-            await redisClient.setEx(userId , (365*24*60*60) , token) 
+        JWT.sign(payload, REFRESH_TOKEN_SECRET_KEY, options, async (err, token) => {
+            if (err) reject(createError.InternalServerError("خطای سروری"));
+            await redisClient.SETEX(String(userId), (365 * 24 * 60 * 60), token);
             resolve(token)
         })
     })
 }
-function verifyRefreshToken(token){
-        return new Promise((resolve , reject) => {
-            JWT.verify(token , REFRESH_TOKEN_SECRET_KEY ,async (err , payload) => {
-                if(err) reject(createHttpError.Unauthorized('try again to access yore account'))
-                const {mobile} = payload || {};
-                const user = await UserModel.findOne({mobile} , {password : 0 , otp : 0})
-                if(!user) reject(createHttpError.Unauthorized('not found any account'))
-                const refreshToken = await redisClient.get(user?._id || 'default_key');
-                if (!refreshToken) reject(createHttpError.Unauthorized('cant access to yore account'))
-                if (token === refreshToken) return resolve(mobile);
-                reject(createHttpError.Unauthorized('cant access to yore account'))
-                
-            })
+function VerifyRefreshToken(token) {
+    return new Promise((resolve, reject) => {
+        JWT.verify(token, REFRESH_TOKEN_SECRET_KEY, async (err, payload) => {
+            if (err) reject(createError.Unauthorized("وارد حساب کاربری خود شوید"))
+            const { mobile } = payload || {};
+            const user = await UserModel.findOne({ mobile }, { password: 0, otp: 0 })
+            if (!user) reject(createError.Unauthorized("حساب کاربری یافت نشد"))
+            const refreshToken = await redisClient.get(String(user?._id));
+            if (!refreshToken) reject(createError.Unauthorized("ورود مجدد به حسابی کاربری انجام نشد"))
+            if (token === refreshToken) return resolve(mobile);
+            reject(createError.Unauthorized("ورود مجدد به حسابی کاربری انجام نشد"))
         })
+    })
+}
+function deleteFileInPublic(fileAddress) {
+    if (fileAddress) {
+        const pathFile = path.join(__dirname, "..", "..", "public", fileAddress)
+        if (fs.existsSync(pathFile)) fs.unlinkSync(pathFile)
     }
-function deleteFileInPublic (fileAddress){
-    if(fileAddress){
-    const pathFile = path.join(__dirname , '..' , '..' , 'public' , fileAddress)
-    if(fs.existsSync(pathFile)) fs.unlinkSync(pathFile)
-    }
-    
-} 
-function listOfImagesFromRequest(files , fileUploadPath){
-    if(files?.length > 0){
-        return ((files.map(file => path.join(fileUploadPath , file.filename))).map(item => item.replace(/\\/g, '/')))
-    }else{
+}
+function ListOfImagesFromRequest(files, fileUploadPath) {
+    if (files?.length > 0) {
+        return ((files.map(file => path.join(fileUploadPath, file.filename))).map(item => item.replace(/\\/g, "/")))
+    } else {
         return []
     }
-
 }
-function setFeatures(){
-    const {colors , width ,height , length , weight} = body;
+function setFeatures(body) {
+    const { colors, width, weight, height, length } = body;
     let features = {};
     features.colors = colors;
-    if (
-      !isNaN(+width) ||
-      !isNaN(+height) ||
-      !isNaN(+length) ||
-      !isNaN(+weight)
-    ) {
-      if (!width) features.width = 0;
-      else features.width = +width;
-      if (!height) features.height = 0;
-      else features.height = +height;
-      if (!length) features.length = 0;
-      else features.length = +length;
-      if (!weight) features.weight = 0;
-      else features.weight = +weight;
+    if (!isNaN(+width) || !isNaN(+height) || !isNaN(+weight) || !isNaN(+length)) {
+        if (!width) features.width = 0;
+        else features.width = +width;
+        if (!height) features.height = 0;
+        else features.height = +height;
+        if (!weight) features.weight = 0;
+        else features.weight = +weight;
+        if (!length) features.length = 0;
+        else features.length = +length;
     }
-        return features
+    return features
 }
-function copyObject(object){
-    return JSON.parse(JSON.stringify(object))
-}
-function deleteInvalidPropertyInObject(data = {} , blackListFields = []){
-    let nullishData = ['',' ' ,'0',0,null,undefined]
+function deleteInvalidPropertyInObject(data = {}, blackListFields = []) {
+    let nullishData = ["", " ", "0", 0, null, undefined]
     Object.keys(data).forEach(key => {
-        if(blackListFields.includes(key)) delete data[key]
-        if(typeof data[key] == 'string') data[key] = data[key].trim();
-        if(Array.isArray(data[key]) && data[key].length > 0) data[key] = data[key].map(item => item.trim())
-          if(Array.isArray(data[key]) && data[key].length == 0) delete data[key]
-        if(nullishData.includes(data[key])) delete data[key];
+        if (blackListFields.includes(key)) delete data[key]
+        if (typeof data[key] == "string") data[key] = data[key].trim();
+        if (Array.isArray(data[key]) && data[key].length > 0) data[key] = data[key].map(item => item.trim())
+        if (Array.isArray(data[key]) && data[key].length == 0) delete data[key]
+        if (nullishData.includes(data[key])) delete data[key];
     })
 }
-function getTime(time) {
-    let total = Math.round(time) / 60;
-    let [min, percentage] = String(total).split(".");
-    if(percentage == undefined) percentage = "0"
-    let sec = Math.round(((percentage.substring(0,2)) * 60) / 100);
-    let hour = 0;
-    if (min > 59) {
-      total = min / 60;
-      [hour , percentage] = String(total).split(".")
-      if(percentage == undefined) percentage = "0"
-      min = Math.round(((percentage.substring(0,2)) * 60) / 100);
+function copyObject(object) {
+    return JSON.parse(JSON.stringify(object))
+}
+function getTime(seconds) {
+    let total = Math.round(seconds) / 60;
+    let [minutes, percent] = String(total).split(".");
+    let second = Math.round((percent * 60) / 100).toString().substring(0, 2);
+    let houre = 0;
+    if (minutes > 60) {
+        total = minutes / 60
+         let [h1, percent] = String(total).split(".");
+         houre = h1,
+         minutes = Math.round((percent * 60) / 100).toString().substring(0, 2);
     }
-    if(hour < 10 ) hour = `0${hour}` ;
-    if(min < 10) min = `0${min}`
-    if(sec < 10) sec = `0${sec}`
-    return hour + ":" + min + ":" + sec;
-  }
-  function getTimeOfCourse(chapters = []) {
-    let time,hour,minute,second = 0;
+    if(String(houre).length ==1) houre = `0${houre}`
+    if(String(minutes).length ==1) minutes = `0${minutes}`
+    if(String(second).length ==1) second = `0${second}`
+    
+    return (houre + ":" + minutes + ":" +second)
+}
+function getTimeOfCourse(chapters = []){
+    let time, hour, minute, second = 0;
     for (const chapter of chapters) {
         if(Array.isArray(chapter?.episodes)){
-        for (const episode of chapter.episode) {
-            if(episode?.time) time = episode.time.split(':')
-                else time = '00:00:00'.split(':')
-            if(time.length == 3){
-                second += Number(time[0]) * 3600 //convert hour to second
-                second += Number(time[1]) * 60 // convert minute to second
-                second += Number(time[2]) //sum second with second
-            }else if(time.length == 2){
+            for (const episode of chapter.episodes) {
+                if(episode?.time) time = episode.time.split(":") // [hour, min, second]
+                else time = "00:00:00".split(":")
+                if(time.length == 3){
+                    second += Number(time[0]) * 3600 // convert hour to second
+                    second += Number(time[1]) * 60 // convert minute to second
+                    second += Number(time[2]) //sum second with seond
+                }else if(time.length == 2){ //05:23
                     second += Number(time[0]) * 60 // convert minute to second
-                    second += Number(time[1]) //sum second with second
+                    second += Number(time[1]) //sum second with seond
                 }
             }
         }
     }
-    hour = Math.floor(second / 3600) //convert second to hour
-    minute = Math.floor(second / 60) % 60 //convert second to minute
-    second = Math.floor(second % 60) //convert seconds to second
-    if(hour < 10 ) hour = `0${hour}` ;
-    if(min < 10) min = `0${min}`
-    if(sec < 10) sec = `0${sec}`
-    return hour + ":" + min + ":" + sec;
-  }
-
-
+    hour = Math.floor(second / 3600); //convert second to hour
+    minute = Math.floor(second / 60) % 60; //convert second to mintutes
+    second = Math.floor(second % 60); //convert seconds to second
+    if(String(hour).length ==1) hour = `0${hour}`
+    if(String(minute).length ==1) minute = `0${minute}`
+    if(String(second).length ==1) second = `0${second}`
+    return (hour + ":" + minute + ":" +second) 
+}
+function calculateDiscount(price, discount){
+    return Number(price) - ((Number(discount) / 100) * Number(price))
+}
+function invoiceNumberGenerator(){
+    return moment().format("jYYYYjMMjDDHHmmssSSS") + String(process.hrtime()[1]).padStart(9, 0)
+}
+async function getBasketOfUser(userID, discount = {}){
+    const userDetail = await UserModel.aggregate([
+        {
+            $match : { _id: userID }
+        },
+        {
+            $project:{ basket: 1}
+        },
+        {
+            $lookup: {
+                from: "products",
+                localField: "basket.products.productID",
+                foreignField: "_id",
+                as: "productDetail"
+            }
+        },
+        {
+            $lookup: {
+                from: "courses",
+                localField: "basket.courses.courseID",
+                foreignField: "_id",
+                as: "courseDetail"
+            }
+        },
+        {
+            $addFields : {
+                "productDetail" : {
+                    $function: {
+                        body: function(productDetail, products){
+                            return productDetail.map(function(product){
+                                const count = products.find(item => item.productID.valueOf() == product._id.valueOf()).count;
+                                const totalPrice = count * product.price
+                                return {
+                                    ...product,
+                                    basketCount: count,
+                                    totalPrice,
+                                    finalPrice: totalPrice - ((product.discount / 100) * totalPrice)
+                                }
+                            })
+                        },
+                        args: ["$productDetail", "$basket.products"],
+                        lang: "js"
+                    }
+                },
+                "courseDetail" : {
+                    $function: {
+                        body: function(courseDetail){
+                            return courseDetail.map(function(course){
+                                return {
+                                    ...course,
+                                    finalPrice: course.price - ((course.discount / 100) * course.price)
+                                }
+                            })
+                        },
+                        args: ["$courseDetail"],
+                        lang: "js"
+                    }
+                },
+                "payDetail" : {
+                    $function: {
+                        body: function(courseDetail, productDetail, products){
+                            const courseAmount =  courseDetail.reduce(function(total, course){
+                                return total + (course.price - ((course.discount / 100) * course.price))
+                            }, 0)
+                            const productAmount =  productDetail.reduce(function(total, product){
+                                const count = products.find(item => item.productID.valueOf() == product._id.valueOf()).count
+                                const totalPrice = count * product.price;
+                                return total + (totalPrice - ((product.discount / 100) * totalPrice))
+                            }, 0)
+                            const courseIds = courseDetail.map(course => course._id.valueOf())
+                            const productIds = productDetail.map(product => product._id.valueOf())
+                            return {
+                                courseAmount,
+                                productAmount,
+                                paymentAmount : courseAmount + productAmount,
+                                courseIds,
+                                productIds
+                            }
+                        },
+                        args: ["$courseDetail", "$productDetail", "$basket.products"],
+                        lang: "js"
+                    }
+                },
+            }
+        },{
+            $project: {
+                basket: 0
+            }
+        }
+    ]);
+    return copyObject(userDetail)
+}
 module.exports = {
-    randomInt ,
-    SignAccessToken ,
-    SignRefreshToken ,
-    verifyRefreshToken ,
-    deleteFileInPublic ,
-    listOfImagesFromRequest ,
-    copyObject ,
-    setFeatures ,
-    deleteInvalidPropertyInObject , 
-    getTime ,
-    getTimeOfCourse
+    RandomNumberGenerator,
+    SignAccessToken,
+    SignRefreshToken,
+    VerifyRefreshToken,
+    deleteFileInPublic,
+    ListOfImagesFromRequest,
+    copyObject,
+    setFeatures,
+    deleteInvalidPropertyInObject,
+    getTime,
+    getTimeOfCourse,
+    calculateDiscount,
+    invoiceNumberGenerator,
+    getBasketOfUser
 }

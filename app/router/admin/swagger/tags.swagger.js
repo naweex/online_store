853d8ab70d1ec622491834d70349943545c60820
navid bahrami <navidbahrami1996@gmail.com>
2024-11-21 16:06:36 +0330

    tags swagger

---
+++ app/router/admin/swagger/tags.swagger.js
@@ -0,0 +1,28 @@
+/**
+ * @swagger
+ *  tags:
+ *      -   name : Admin-Panel
+ *          description : action of admin (add, remove, edit  and any do)
+ *      -   name : Discount(AdminPanel)
+ *          description : action of discount (add, remove)
+ *      -   name: Transactions(AdminPanel)
+ *          description: get And view All transactions of users
+ *      -   name : RBAC(AdminPanel)
+ *          description : RoleBaseAccessControl System create and manage role and permissions 
+ *      -   name : Users(AdminPanel)
+ *          description : management users of website 
+ *      -   name : Course(AdminPanel)
+ *          description : management course section like manage episode, chapter and courses
+ *      -   name : Chapter(AdminPanel)
+ *          description : management Chapters section
+ *      -   name : Episode(AdminPanel)
+ *          description : the videos(episode) of Chapter section
+ *      -   name: Product(AdminPanel)
+ *          description : management product routes
+ *      -   name: Blog(AdminPanel)
+ *          description: made blog management admin panel 
+ *      -   name: Category(AdminPanel)
+ *          description: all method and routes about category section
+ *      -   name: Prisma(Api)
+ *          description: create some api's with prisma and postgreSQL category section
+ */
